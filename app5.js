9baa1764f22cbc4a8300dedc9e5d10f25a51003e
Ryu Takemoto <ryu3ta.ke.mo100307@gmail.com>
2024-11-04 11:40:59 +0900

    じゃんけんの勝敗の変更

--- app5.js
+++ app5.js
@@ -32,16 +32,30 @@ app.get("/janken", (req, res) => {
   let win = Number( req.query.win );
   let total = Number( req.query.total );
   console.log( {hand, win, total});
+
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
   if( num==1 ) cpu = 'グー';
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
+
   let judgement = '勝ち';
-  win += 1;
-  total += 1;
+
+ if (hand === cpu) {
+   judgement = '引き分け';
+ } else if (
+   (hand === 'グー' && cpu === 'チョキ') ||
+   (hand === 'チョキ' && cpu === 'パー') ||
+   (hand === 'パー' && cpu === 'グー')
+ ) {
+   judgement = '勝ち';
+   win += 1;
+ } else {
+   judgement = '負け';
+ }
+
+ total += 1;
+
   const display = {
     your: hand,
     cpu: cpu,
